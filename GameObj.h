f18446b1a21016503aa05b4955886d7e559cf92c
nordsoft <nordsoft@rambler.ru>
2014-10-19 22:51:53 +0400

    BaseApplication
    Copy cocos2d example and make basic architecture

---
+++ GameObj.h
@@ -0,0 +1,36 @@
+//
+//  GameObj.h
+//  MagicWars
+//
+//  Created by nordsoft on 19.10.14.
+//
+//
+
+#ifndef MagicWars_GameObj_h
+#define MagicWars_GameObj_h
+
+#include <cocos2d.h>
+#include <string>
+
+namespace MagicWars_NS {
+    class GameObj
+    {
+    public:
+        GameObj(const std::string i_spr)
+        {
+            d_sprite = cocos2d::Sprite::create(i_spr);
+        }
+        
+        void born(cocos2d::Layer *io_layer, cocos2d::Vec2 i_position)
+        {
+            io_layer->addChild(d_sprite);
+            d_sprite->setPosition(i_position);
+        }
+        
+    protected:
+        cocos2d::Sprite *d_sprite = nullptr;
+    };
+}
+
+
+#endif
---
+++ Magican.h
@@ -0,0 +1,23 @@
+//
+//  Magican.h
+//  MagicWars
+//
+//  Created by nordsoft on 19.10.14.
+//
+//
+
+#ifndef MagicWars_Magican_h
+#define MagicWars_Magican_h
+
+#include "GameObj.h"
+
+namespace MagicWars_NS
+{
+    class Magican: public GameObj
+    {
+    public:
+        Magican(): GameObj("light1.png") {}
+    };
+}
+
+#endif
