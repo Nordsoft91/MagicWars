400cbcaa4e0b022a92ea71f775295c91b938f694
nordsoft <nordsoft@rambler.ru>
2015-11-10 00:10:39 +0300

    [MW3] Fix bug with change to deleted team

---
+++ Controllers/TurnController.cpp
@@ -0,0 +1,150 @@
+//
+//  TurnController.cpp
+//  MagicWars
+//
+//  Created by nordsoft on 09.11.15.
+//
+//
+
+#include "TurnController.h"
+
+namespace MagicWars_NS {
+    bool TurnController::insert(Magican* i_char, const std::string& i_side)
+    {
+        d_persons[i_char] = TurnInfo{i_side, true, TURN_MOVE | TURN_ATTACK};
+        //add side if new
+        if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
+            d_sides.push_back(i_side);
+        
+        d_iterSideTurn = d_sides.begin();
+        return true;
+    }
+    
+    bool TurnController::remove(Magican* i_char)
+    {
+        std::string hisSide = d_persons[i_char].d_side;
+        d_persons.erase(i_char);
+
+        return true;
+    }
+    
+    bool TurnController::isTurn(Magican* i_char, int i_status)
+    {
+        d_persons[i_char].d_alive = i_char->isAlive();
+        if(   d_persons[i_char].d_side == *d_iterSideTurn
+           && d_persons[i_char].d_alive
+           && (d_persons[i_char].d_active & i_status) )
+            return true;
+        return false;
+    }
+    
+    bool TurnController::isTurn(int i_status)
+    {
+        return isTurn(d_turn, i_status);
+    }
+    
+    bool TurnController::beginTurn(Magican* i_char, int i_status)
+    {
+        if(!isTurn(i_char, i_status))
+            return false;
+        
+        if(d_turn)
+            d_turn->setActive(false);
+        d_turn = i_char;
+        d_turn->setActive(true);
+        return true;
+    }
+    
+    bool TurnController::endTurn(int i_status)
+    {
+        
+        d_persons[d_turn].d_active &= ~i_status;
+        std::vector<Magican*> vec = sideArray(d_persons[d_turn].d_side);
+        if(d_persons[d_turn].d_active == 0 || !d_persons[d_turn].d_alive)
+        {
+            d_turn->setActive(false);
+            d_turn = nullptr;
+        }
+        
+        for( auto i : vec )
+        {
+            d_persons[i].d_alive = i->isAlive();
+            if( d_persons[i].d_alive && d_persons[i].d_active>0 )
+            {
+                return true;
+            }
+        }
+        //change side
+        do
+        {
+            if( ++d_iterSideTurn == d_sides.end() )
+                d_iterSideTurn = d_sides.begin();
+        }
+        while(sideArray(*d_iterSideTurn).empty());
+        
+        //make team active
+        vec = sideArray(*d_iterSideTurn);
+        for( auto i : vec )
+        {
+            d_persons[i].d_active = TURN_MOVE | TURN_ATTACK;
+            i->increaseMind();
+        }
+        return false;
+    }
+    
+    Magican* TurnController::getTurn() const
+    {
+        return d_turn;
+    }
+    
+    std::string TurnController::getTurnSide() const
+    {
+        return *d_iterSideTurn;
+    }
+    
+    std::vector<Magican*> TurnController::sideArrayActive(const std::string& i_side, int i_status )
+    {
+        std::vector<Magican*> res;
+        if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
+            return res;
+        
+        for( auto& i : d_persons )
+        {
+            if(i.first->isAlive() && i.second.d_side == i_side && i.second.d_active & i_status)
+            {
+                res.push_back(i.first);
+            }
+        }
+        return res;
+    }
+    
+    std::vector<Magican*> TurnController::sideArray(const std::string& i_side)
+    {
+        std::vector<Magican*> res;
+        if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
+            return res;
+        
+        for( auto i = d_persons.begin(); i!=d_persons.end(); ++i )
+        {
+            if(i->first->isAlive() && i->second.d_side == i_side)
+            {
+                res.push_back(i->first);
+            }
+        }
+        return res;
+    }
+    
+    std::vector<Magican*> TurnController::otherSidesArray(const std::string& i_side) const
+    {
+        std::vector<Magican*> res;
+        if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
+            return res;
+        
+        for( auto i = d_persons.begin(); i!=d_persons.end(); ++i )
+        {
+            if(i->second.d_side != i_side)
+                res.push_back(i->first);
+        }
+        return res;
+    }
+}
--- Controllers/TurnController.h
+++ Controllers/TurnController.h
@@ -34,154 +34,33 @@ namespace MagicWars_NS {
         
         ~TurnController() = default;
         
-        bool insert(Magican* i_char, const std::string i_side)
-        {
-            d_persons[i_char] = TurnInfo{i_side, true, TURN_MOVE | TURN_ATTACK};
-            //add side if new
-            if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
-                d_sides.push_back(i_side);
-            
-            d_iterSideTurn = 0;
-            return true;
-        }
+        bool insert(Magican* i_char, const std::string& i_side);
         
-        bool remove(Magican* i_char)
-        {
-            std::string hisSide = d_persons[i_char].d_side;
-            d_persons.erase(i_char);
-            //remove side if not exist
-            sideArray(hisSide);
-            return true;
-        }
+        bool remove(Magican* i_char);
         
-        bool isTurn(Magican* i_char, int i_status)
-        {
-            d_persons[i_char].d_alive = i_char->isAlive();
-            if(   d_persons[i_char].d_side == d_sides[d_iterSideTurn]
-               && d_persons[i_char].d_alive
-               && (d_persons[i_char].d_active & i_status) )
-                return true;
-            return false;
-        }
+        bool isTurn(Magican* i_char, int i_status);
         
-        bool isTurn(int i_status)
-        {
-            return isTurn(d_turn, i_status);
-        }
+        bool isTurn(int i_status);
         
-        bool beginTurn(Magican* i_char, int i_status)
-        {
-            if(!isTurn(i_char, i_status))
-                return false;
-            
-            if(d_turn)
-                d_turn->setActive(false);
-            d_turn = i_char;
-            d_turn->setActive(true);
-            return true;
-        }
+        bool beginTurn(Magican* i_char, int i_status);
         
-        bool endTurn(int i_status)
-        {
-            
-            d_persons[d_turn].d_active &= ~i_status;
-            std::vector<Magican*> vec = sideArray(d_persons[d_turn].d_side);
-            if(d_persons[d_turn].d_active == 0 || !d_persons[d_turn].d_alive)
-            {
-                d_turn->setActive(false);
-                d_turn = nullptr;
-            }
-            
-            for( auto i : vec )
-            {
-                d_persons[i].d_alive = i->isAlive();
-                if( d_persons[i].d_alive && d_persons[i].d_active>0 )
-                {
-                    return true;
-                }
-            }
-            //change side
-            if( ++d_iterSideTurn == d_sides.size() )
-                d_iterSideTurn = 0;
-            //make team active
-            vec = sideArray(d_sides.at(d_iterSideTurn));
-            for( auto i : vec )
-            {
-                d_persons[i].d_active = TURN_MOVE | TURN_ATTACK;
-                i->increaseMind();
-            }
-            return false;
-        }
+        bool endTurn(int i_status);
         
-        Magican* getTurn() const
-        {
-            return d_turn;
-        }
+        Magican* getTurn() const;
         
-        std::string getTurnSide() const
-        {
-            return d_sides.at(d_iterSideTurn);
-        }
+        std::string getTurnSide() const;
         
-        std::vector<Magican*> sideArrayActive(const std::string i_side, int i_status = TURN_ANY)
-        {
-            std::vector<Magican*> res;
-            if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
-                return res;
-            
-            for( auto i = d_persons.begin(); i!=d_persons.end(); ++i )
-            {
-                if(i->first->isAlive() && i->second.d_side == i_side)
-                {
-                    if(i->second.d_active | i_status)
-                        res.push_back(i->first);
-                }
-            }
-            if(res.empty())
-                d_sides.erase(std::find(d_sides.begin(), d_sides.end(), i_side));
-            
-            return res;
-        }
+        std::vector<Magican*> sideArrayActive(const std::string& i_side, int i_status = TURN_ANY);
         
-        std::vector<Magican*> sideArray(const std::string i_side)
-        {
-            std::vector<Magican*> res;
-            if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
-                return res;
-            
-            for( auto i = d_persons.begin(); i!=d_persons.end(); ++i )
-            {
-                if(i->first->isAlive() && i->second.d_side == i_side)
-                {
-                    res.push_back(i->first);
-                }
-            }
-            if(res.empty())
-                d_sides.erase(std::find(d_sides.begin(), d_sides.end(), i_side));
-
-            return res;
-        }
+        std::vector<Magican*> sideArray(const std::string& i_side);
         
-        std::vector<Magican*> otherSidesArray(const std::string i_side) const
-        {
-            std::vector<Magican*> res;
-            if(std::find(d_sides.begin(), d_sides.end(), i_side) == d_sides.end())
-                return res;
-            
-            for( auto i = d_persons.begin(); i!=d_persons.end(); ++i )
-            {
-                if(i->second.d_side != i_side)
-                    res.push_back(i->first);
-            }
-            
-            return res;
-        }
+        std::vector<Magican*> otherSidesArray(const std::string& i_side) const;
         
     private:
         
         std::map<Magican*, TurnInfo> d_persons;
         std::vector<std::string> d_sides;
-        size_t d_iterSideTurn = 0;
+        std::vector<std::string>::iterator d_iterSideTurn;
         
         Magican* d_turn = nullptr;
     };
