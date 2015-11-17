2af95e3a9c748bfefa583bf5625e6cfdcc291fcd
Nordsoft91 <nordsoft@rambler.ru>
2015-11-17 16:32:59 +0300

    Update UIEvent.h

--- Interface/UIEvent.h
+++ Interface/UIEvent.h
@@ -22,7 +22,6 @@ namespace UI_NS {
     {
     public:
         virtual ~Event() = default;
-        
         virtual void throwEvent() = 0;
     };
     
@@ -32,23 +31,9 @@ namespace UI_NS {
     {
     public:
         EventHeap(std::list<Event*> i_events): d_events(i_events) {}
-        
         void releaseResourceControl(Event* i_event) {d_preventRelease.push_back(i_event);}
-        
-        virtual void throwEvent() override
-        {
-            for(auto* i : d_events)
-                i->throwEvent();
-        }
-        
-        virtual ~EventHeap()
-        {
-            for(auto* i : d_events)
-            {
-                if(std::find(d_preventRelease.begin(), d_preventRelease.end(), i)==d_preventRelease.end())
-                    delete i;
-            }
-        }
+        virtual void throwEvent() override;
+        virtual ~EventHeap();
         
     private:
         std::list<Event*> d_events;
@@ -62,12 +47,7 @@ namespace UI_NS {
         EventMessage(cocos2d::Node* io_scene, const std::list<std::string>& i_message): d_scene(io_scene), d_message(i_message)
         {}
         
-        virtual void throwEvent() override
-        {
-            cocos2d::Size visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
-            cocos2d::Vec2 screenCenter(visibleSize.width/2, visibleSize.height/2);
-            d_scene->addChild(UI_NS::MessageSequence::create(screenCenter, cocos2d::Color4F{0,0,0,0.5}, d_message));
-        }
+        virtual void throwEvent() override;
         
     protected:
         cocos2d::Node* d_scene = nullptr;
@@ -80,11 +60,7 @@ namespace UI_NS {
         EventDialog(cocos2d::Node* io_character, const std::list<std::string>& i_message): d_owner(io_character), d_message(i_message)
         {}
         
-        virtual void throwEvent() override
-        {
-            MagicWars_NS::TouchControl::instance().centralizeOn(d_owner->getPosition());
-            d_owner->addChild(UI_NS::MessageSequence::create(cocos2d::Vec2(64,128), cocos2d::Color4F{1,1,1,0.5}, d_message));
-        }
+        virtual void throwEvent() override;
         
     protected:
         cocos2d::Node *d_owner = nullptr, *d_scene = nullptr;
@@ -96,7 +72,6 @@ namespace UI_NS {
     {
     public:
         EventActivateTrigger(Trigger* i_trigger): d_trigger(i_trigger) {}
-        
         virtual void throwEvent() override;
         
     protected:
@@ -107,16 +82,8 @@ namespace UI_NS {
     class EventCondition: public Event, public Condition
     {
     public:
-        
-        virtual bool get() const override
-        {
-            return d_active;
-        }
-        
-        virtual void throwEvent() override
-        {
-            d_active = true;
-        }
+        virtual bool get() const override;
+        virtual void throwEvent() override;
         
     protected:
         bool d_active = false;
