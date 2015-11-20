878d16d410dc24eb39afd936a7c489999856f882
nordsoft <nordsoft@rambler.ru>
2015-11-21 00:17:08 +0300

    [MW44] Migrate to cocos2d-x 3.9

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -103,14 +103,8 @@ void HelloWorld::update(float d_time)
 
 void HelloWorld::menuCloseCallback(Ref* pSender)
 {
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_WP8) || (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT)
-	MessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
-    return;
-#endif
-
-    cocos2d::log("END GAME");
     Director::getInstance()->end();
-
+    
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
