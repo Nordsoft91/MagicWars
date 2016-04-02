09f593b6e97bc3951625330558dd1f6ec707c3f3
nordsoft <nordsoft@rambler.ru>
2016-04-02 21:26:20 +0300

    UIInformation prototype

---
+++ Classes/Interface/UIInformation.cpp
@@ -0,0 +1,9 @@
+//
+//  UIInformation.cpp
+//  MagicWars
+//
+//  Created by nordsoft on 02.04.16.
+//
+//
+
+#include "UIInformation.h"
---
+++ Classes/Interface/UIInformation.h
@@ -0,0 +1,21 @@
+//
+//  UIInformation.h
+//  MagicWars
+//
+//  Created by nordsoft on 02.04.16.
+//
+//
+
+#ifndef __MagicWars__UIInformation__
+#define __MagicWars__UIInformation__
+
+#include <Controllers/TouchControl.h>
+
+namespace UI_NS {
+    class InformationScreen
+    {
+        
+    };
+}
+
+#endif /* defined(__MagicWars__UIInformation__) */
