827dfad691f76b83af7284b9e389a290ed4718f4
nordsoft <nordsoft@rambler.ru>
2021-12-22 01:56:47 +0300

    Add resources extraction in install script

--- patch.sh
+++ patch.sh
@@ -4,6 +4,9 @@ if [[ "$0" = /* ]]
 then
 	echo "You must call ./patch.sh in working directory"
 else
+	tar -xvzf ./import.tar import
 	cp -R patch/* cocos2d
+	cp -R import/* Resources
+	rm -rf import
 	echo "Patch complete"
 fi
